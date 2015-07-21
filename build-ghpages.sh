c1667be277a3d38c0684d01ee6c52e6922ee2f58
Philippe Latulippe <philippe.latulippe@gmail.com>
2015-07-21 13:55:44 +0200

    finish working on gh-pages script

--- build-ghpages.sh
+++ build-ghpages.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+#This script operates on the git repository and doesn't check whether what
+#it's doing is correct. Take a look at it before running it.
+
 set -e
 set -o pipefail
 
 git checkout gh-pages
+git reset --hard HEAD~1
+git pull . master
 markdown Encrypted-email-on-Android.md | cat header.html - footer.html > index.html 
 git add index.html
 echo "You are now in the gh-pages branch, and index.html has been staged."
